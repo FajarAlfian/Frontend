88de967ae223c8036407cb5c8c36f52de55f2a62
Muti Salsabila <mutislbla@gmail.com>
2025-06-11 14:02:21 +0700

    add component statistics

---
+++ src/components/molecules/statistics.jsx
@@ -0,0 +1,42 @@
+import { Stack, Typography } from "@mui/material";
+
+const Statistics = () => {
+  return (
+    <Stack
+      justifyContent="center"
+      textAlign="center"
+      padding={20}
+      direction={{ xs: "column", sm: "column", md: "row" }}
+      spacing={{ xs: 3, sm: 5, md: 10 }}
+    >
+      <Stack sx={{ width: { xs: "100%", md: "33.33%" } }} spacing={5}>
+        <Typography variant="h2" color="dlang.green" fontWeight="1000">
+          100+
+        </Typography>
+        <Typography variant="h5" color="dlang.grey" fontWeight="600">
+          Choose the class you like and get the skills
+        </Typography>
+      </Stack>
+
+      <Stack sx={{ width: { xs: "100%", md: "33.33%" } }} spacing={5}>
+        <Typography variant="h2" color="dlang.green" fontWeight="1000">
+          50+
+        </Typography>
+        <Typography variant="h5" color="dlang.grey" fontWeight="600">
+          Having teachers who are highly skilled and competent in the language
+        </Typography>
+      </Stack>
+
+      <Stack sx={{ width: { xs: "100%", md: "33.33%" } }} spacing={5}>
+        <Typography variant="h2" color="dlang.green" fontWeight="1000">
+          10+
+        </Typography>
+        <Typography variant="h5" color="dlang.grey" fontWeight="600">
+          Many alumni become ministry employees because of their excelent
+          language skills
+        </Typography>
+      </Stack>
+    </Stack>
+  );
+};
+export default Statistics;
