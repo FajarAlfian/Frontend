4cf9fa632802e56141ef45626af3d3bc83ab448d
Muti Salsabila <mutislbla@gmail.com>
2025-06-12 10:36:16 +0700

    feat: add benefit section in landing page

---
+++ src/components/molecules/benefit.jsx
@@ -0,0 +1,34 @@
+import { Box, Grid, Typography } from "@mui/material";
+
+import People from "../../assets/people.png";
+const Benefit = () => {
+  return (
+    <Grid
+      container
+      backgroundColor="dlang.orange"
+      px={20}
+      py={10}
+      direction="row"
+      spacing={10}
+    >
+      <Grid color="white" size={8} textAlign="justify" alignContent="center">
+        <Typography variant="h3" fontWeight="bold" marginY={4}>
+          Gets your best benefit
+        </Typography>
+        <Typography variant="h5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+          rem? Quia quis officiis sunt nam cumque. Deserunt, at blanditiis,
+          dolor id adipisci illo nisi pariatur, architecto voluptatibus quisquam
+          eum odio. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Provident, rem? Quia quis officiis sunt nam cumque. Deserunt, at
+          blanditiis, dolor id adipisci illo nisi pariatur, architecto
+          voluptatibus quisquam eum odio.
+        </Typography>
+      </Grid>
+      <Grid size={4} justifyContent="center" alignContent="center">
+        <img src={People} width="100%" justifyContent="center" alt="" />
+      </Grid>
+    </Grid>
+  );
+};
+export default Benefit;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,14 @@ import Navbar from "../components/molecules/navbar";
 import Footer from "../components/molecules/footer";
 import Banner from "../components/molecules/banner";
 import Statistics from "../components/molecules/statistics";
+import Benefit from "../components/molecules/benefit";
 const Home = () => {
   return (
     <>
       <Navbar />
       <Banner />
       <Statistics />
+      <Benefit />
       <Footer />
     </>
   );
