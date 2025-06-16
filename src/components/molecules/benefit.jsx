279658bed02e4f8b946b7fef8bca5680b97ac09f
FajarAlfian <fajaralfian231@gmail.com>
2025-06-16 18:18:52 +0700

    fix: responsive benefit

--- src/components/molecules/benefit.jsx
+++ src/components/molecules/benefit.jsx
@@ -1,21 +1,25 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-
 import People from "../../assets/people.png";
 const Benefit = () => {
   return (
     <Grid
       container
       backgroundColor="dlang.orange"
-      px={20}
+      px={{ xs: 3, sm: 5, md: 20 }}
       py={5}
-      direction="row"
+      direction={{ xs: "column", md: "row" }}
       spacing={10}
-      alignItems="flex-start"
+      alignItems="center"
       marginTop={"124px"}
       marginBottom={"122px"}
+
     >
-      <Grid color="white" size={8} textAlign="justify" alignContent="center">
+      <Grid xs={12} color="white" size={8} textAlign="justify" alignContent="center" sx={{
+        display: { xs: "flex", md: "block" }, 
+        flexDirection: { xs: "column", md: "row" },
+        justifyContent: { xs: "center", md: "flex-start" }, 
+      }}>
         <Typography variant="h3" fontWeight="bold" marginY={4}>
           Gets your best benefit
         </Typography>
@@ -29,8 +33,13 @@ const Benefit = () => {
           voluptatibus quisquam eum odio.
         </Typography>
       </Grid>
-      <Grid size={4} justifyContent="center" alignContent="center">
-        <img src={People} width="100%" justifycontent="center" alt="" />
+      <Grid xs={12} size={4} justifyContent="center" alignContent="center">
+        <img
+          src={People}
+          width="100%"  
+          alt="People"
+          style={{ objectFit: "contain" }}  
+        />
       </Grid>
     </Grid>
   );
