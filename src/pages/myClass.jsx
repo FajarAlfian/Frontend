0dc19467f9b8331d1f0e69fb74bb347313ba892f
muti <mutislbla@gmail.com>
2025-07-03 23:51:16 +0700

    fix: my class

--- src/pages/myClass.jsx
+++ src/pages/myClass.jsx
@@ -5,29 +5,27 @@ import CardMedia from "@mui/material/CardMedia";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
-const courses = [
-  {
-    id: 1,
-    image: "/assets/courseimage/english_junior.png",
-    category: "English",
-    title: "Basic English for Junior",
-    price: "IDR 400.000",
-    link: "",
-  },
-  {
-    id: 2,
-    image: "/assets/courseimage/english_expert.png",
-    category: "English",
-    title: "Complit Package - Expert English, TOEFL and IELTS",
-    price: "IDR 2.000.000",
-    link: "",
-  },
-];
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { ConvertDate, formatRupiah } from "../utils/util";
 
 const MyClass = () => {
+  const [course, setCourse] = useState([]);
+  useEffect(() => {
+    const token = Cookies.get("token");
+    axios
+      .get("http://localhost:5009/api/Courses/paid", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setCourse(res.data.data);
+      })
+      .catch((err) => console.error("Fetch invoice error:", err));
+  }, []);
   return (
     <>
-      {courses.map((courses) => (
+      {course.map((courses) => (
         <Box mx={10}>
           <Grid
             container
@@ -45,8 +43,8 @@ const MyClass = () => {
               >
                 <CardMedia
                   component="img"
-                  image={courses.image}
-                  alt={courses.title}
+                  image={courses.course_image}
+                  alt={courses.course_name}
                 />
               </Card>
             </Grid>
@@ -56,19 +54,19 @@ const MyClass = () => {
                   fontSize={{ xs: "8", sm: "16px" }}
                   sx={{ fontWeight: "400", color: "#828282" }}
                 >
-                  {courses.category}
+                  {courses.category_name}
                 </Typography>
                 <Typography
                   fontSize={{ xs: "12", sm: "24px" }}
                   sx={{ fontWeight: "600", color: "#333333" }}
                 >
-                  {courses.title}
+                  {courses.course_name}
                 </Typography>
                 <Typography
                   fontSize={{ xs: "10", sm: "20px" }}
                   sx={{ fontWeight: "400", color: "#EA9E1F" }}
                 >
-                  Schedule: Friday, 29 Juli 2022
+                  Schedule: {courses.schedule_date}
                 </Typography>
               </Stack>
             </Grid>
