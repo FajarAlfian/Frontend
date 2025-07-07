bd7ab252f6ecbb10da90d4389cd23584893f1139
FajarAlfian <fajaralfian231@gmail.com>
2025-07-07 11:46:58 +0700

    feat: hamburger menu

--- src/components/molecules/navbar.jsx
+++ src/components/molecules/navbar.jsx
@@ -1,40 +1,190 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 import AppBar from "@mui/material/AppBar";
-import { Link } from "react-router-dom";
-import logo from "../../assets/Logo.png";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PersonIcon from "@mui/icons-material/Person";
 import LogoutIcon from "@mui/icons-material/Logout";
-import Divider from "@mui/material/Divider";
-import Cookies from "js-cookie";
+
+import logo from "../../assets/Logo.png";
+
 const Navbar = () => {
-  const isUserLoggedIn = Cookies.get("token") !== undefined;
+  const navigate = useNavigate();
+  const isUserLoggedIn = !!Cookies.get("token");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width:900px)");
+
   const handleLogout = () => {
     Cookies.remove("token");
     navigate("/");
   };
+
+  const menuList = (
+    <>
+      <IconButton
+        component={Link}
+        to="/checkout"
+        sx={{ color: "dlang.green", mr: 4 }}
+      >
+        <ShoppingCartIcon />
+      </IconButton>
+      <Button
+        component={Link}
+        to="/class"
+        variant="text"
+        sx={{
+          fontSize: 16,
+          color: "dlang.green",
+          textTransform: "none",
+          mr: 5,
+        }}
+      >
+        My Class
+      </Button>
+      <Button
+        component={Link}
+        to="/invoice"
+        variant="text"
+        sx={{
+          fontSize: 16,
+          color: "dlang.green",
+          textTransform: "none",
+          mr: 5,
+        }}
+      >
+        Invoice
+      </Button>
+      <Divider
+        orientation="vertical"
+        flexItem
+        sx={{ borderColor: "dlang.green", mr: 5 }}
+      />
+      <IconButton component={Link} to="/" sx={{ color: "dlang.green", mr: 2 }}>
+        <PersonIcon />
+      </IconButton>
+      <IconButton sx={{ color: "#EB5757" }} onClick={handleLogout}>
+        <LogoutIcon />
+      </IconButton>
+    </>
+  );
+
+  const mobileMenuList = (
+    <>
+      <Box display="flex" justifyContent="flex-end" width="100%">
+        <IconButton onClick={() => setDrawerOpen(false)}>
+          <CloseIcon />
+        </IconButton>
+      </Box>
+
+      <Button
+        component={Link}
+        to="/checkout"
+        variant="text"
+        onClick={() => setDrawerOpen(false)}
+        sx={{
+          width: "100%",
+          justifyContent: "flex-start",
+          textTransform: "none",
+          mb: 1,
+        }}
+      >
+        <ShoppingCartIcon sx={{ mr: 1, color: "dlang.green" }} />
+        <Typography sx={{ color: "dlang.green" }}>Cart</Typography>
+      </Button>
+
+      <Button
+        component={Link}
+        to="/class"
+        variant="text"
+        onClick={() => setDrawerOpen(false)}
+        sx={{
+          width: "100%",
+          justifyContent: "flex-start",
+          textTransform: "none",
+          mb: 1,
+          color: "dlang.green",
+        }}
+      >
+        My Class
+      </Button>
+
+      <Button
+        component={Link}
+        to="/invoice"
+        variant="text"
+        onClick={() => setDrawerOpen(false)}
+        sx={{
+          width: "100%",
+          justifyContent: "flex-start",
+          textTransform: "none",
+          mb: 2,
+          color: "dlang.green",
+        }}
+      >
+        Invoice
+      </Button>
+
+      <Divider sx={{ width: "100%", mb: 2 }} />
+
+      <Button
+        component={Link}
+        to="/"
+        variant="text"
+        onClick={() => setDrawerOpen(false)}
+        sx={{
+          width: "100%",
+          justifyContent: "flex-start",
+          textTransform: "none",
+          mb: 1,
+          color: "dlang.green",
+        }}
+      >
+        <PersonIcon sx={{ mr: 1 }} />
+        Profile
+      </Button>
+      <Button
+        variant="text"
+        onClick={() => {
+          handleLogout();
+          setDrawerOpen(false);
+        }}
+        sx={{
+          width: "100%",
+          justifyContent: "flex-start",
+          textTransform: "none",
+          color: "#EB5757",
+        }}
+      >
+        <LogoutIcon sx={{ mr: 1 }} />
+        Logout
+      </Button>
+    </>
+  );
+
   return (
-    <Box mx={10}>
+    <Box mx={isMobile ? 1 : 10}>
       <AppBar position="static" color="transparent" elevation={0}>
         <Toolbar
-          sx={{
-            justifyContent: "space-between",
-            paddingLeft: 0,
-            paddingRight: 0,
-          }}
+          sx={{ justifyContent: "space-between", px: 0, py: 0 }}
         >
           <Box display="flex" alignItems="center">
             <Box
               component="img"
               src={logo}
               alt="D Language"
-              sx={{ width: 40, height: 40 }}
-              mr={2}
+              sx={{ width: 40, height: 40, mr: 2 }}
             />
             <Typography
               component={Link}
@@ -44,75 +194,41 @@ const Navbar = () => {
                 fontWeight: 500,
                 color: "dlang.black",
                 textDecoration: "none",
-                display: "block",
               }}
             >
               Language
             </Typography>
           </Box>
+
           {isUserLoggedIn ? (
-            <Box display="flex" alignItems="center">
-              <IconButton
-                component={Link}
-                to="/checkout"
-                sx={{ color: "dlang.green", marginRight: 4 }}
-              >
-                <ShoppingCartIcon />
-              </IconButton>
+            isMobile ? (
+              <>
+                <IconButton
+                  edge="end"
+                  onClick={() => setDrawerOpen(true)}
+                  sx={{ color: "dlang.green" }}
+                >
+                  <MenuIcon />
+                </IconButton>
 
-              <Button
-                component={Link}
-                to="/class"
-                variant="text"
-                sx={{
-                  fontSize: "16px",
-                  color: "dlang.green",
-                  textTransform: "none",
-                  marginRight: 5,
-                }}
-              >
-                My Class
-              </Button>
-              <Button
-                component={Link}
-                to="/invoice"
-                variant="text"
-                sx={{
-                  fontSize: "16px",
-                  color: "dlang.green",
-                  textTransform: "none",
-                  marginRight: 5,
-                }}
-              >
-                Invoice
-              </Button>
-              <Divider
-                orientation="vertical"
-                variant="middle"
-                flexItem
-                sx={{
-                  color: "dlang.green",
-                  border: "1px solid",
-                  marginRight: 5,
-                }}
-              />
-
-              <IconButton
-                component={Link}
-                to="/"
-                sx={{ color: "dlang.green", mr: 2 }}
-              >
-                <PersonIcon />
-              </IconButton>
-
-              <IconButton
-                component={Link}
-                to="/register"
-                sx={{ color: "#EB5757" }}
-              >
-                <LogoutIcon onClick={handleLogout} />
-              </IconButton>
-            </Box>
+                <Drawer
+                  anchor="right"
+                  open={drawerOpen}
+                  onClose={() => setDrawerOpen(false)}
+                >
+                  <Box
+                    sx={{ width: 280, p: 2 }}
+                    role="presentation"
+                    display="flex"
+                    flexDirection="column"
+                  >
+                    {mobileMenuList}
+                  </Box>
+                </Drawer>
+              </>
+            ) : (
+              <Box display="flex" alignItems="center">{menuList}</Box>
+            )
           ) : (
             <Box>
               <Button
