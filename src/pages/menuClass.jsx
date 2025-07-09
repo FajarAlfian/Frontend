dcf3ac01a137f564f0b06fbc73df6d9366c5a5ef
FajarAlfian <fajaralfian231@gmail.com>
2025-07-09 19:45:13 +0700

    filter category by id

--- src/pages/menuClass.jsx
+++ src/pages/menuClass.jsx
@@ -2,23 +2,25 @@ import React, { useEffect, useState } from "react";
 import CourseListDetail from "../components/molecules/courseListDetail";
 import ImageText from "../components/molecules/imageText";
 import Divider from "@mui/material/Divider";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";    
 import axios from "axios";
+
 const MenuClass = () => {
   const BASE_URL = import.meta.env.VITE_API;
   const { nama } = useParams();
   const [language, setLanguage] = useState(null);
+
   useEffect(() => {
     axios
       .get(`${BASE_URL}/Categories/category/${nama}`)
       .then((response) => {
-        console.log(response);
         setLanguage(response.data.data);
       })
       .catch((error) => {
         console.error("Error fetching category:", error);
       });
   }, [nama]);
+
   return (
     <div>
       {language ? (
@@ -29,7 +31,11 @@ const MenuClass = () => {
             foto={language.category_banner}
           />
           <Divider sx={{ marginTop: "80px", marginBottom: "80px" }} />
-          <CourseListDetail limit={3} />
+          <CourseListDetail
+            limit={6}
+            name={language.category_name}
+            categoryId={language.category_id}
+          />
         </>
       ) : (
         <div>Loading...</div>
